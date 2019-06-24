77d3d8136792b3a9de5108310ee7d06460b6ca0e
StyeEye <ColonialBuff@protonmail.com>
2019-06-24 14:22:18 -0600

    Initial setup

---
+++ server.js
@@ -0,0 +1,39 @@
+// Imports
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const session = require('express-session');
+require('dotenv').config();
+
+// Setup
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+// Database setup
+massive(process.env.CONNECTION_STRING)
+    .then(database => {
+        app.set("db", database);
+        console.log("Database connected");
+    })
+    .catch(error => {
+        console.log(`Database connection failed: ${error}`);
+    });
+
+// Endpoints
+
+// Startup
+const port = process.env.PORT || 8090;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
